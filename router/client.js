0bca4cc5bc0192ece362cad020cb27aef3c2724c
jitesh <jiteshdhurat@gmail.com>
2024-05-26 16:57:14 +0530

    Mongo_code changes

--- router/client.js
+++ router/client.js
@@ -1,50 +1,21 @@
-module.exports = function(app,fs)
-{
-
-app.post('/api/client/v1/create_client/',function(req, res){
-
-    let f1 = require(__dirname + './../src/client/create_client');
-    f1.main(req,res,fs)
-});
-
-app.get('/api/client/v1/:e_id/',function(req, res){
-    let f1 = require(__dirname + './../src/client/get_client');
-    f1.main(req,res,fs)
-});
-
-app.get('/api/client/v1/get_client_list/',function(req, res){
-   let f1 = require(__dirname + './../src/client/get_client_list');
-   f1.main(req,res,fs)
-});
-
-app.get('/api/client/v1/write_fileclient/',function(req, res){
-    let f1 = require(__dirname + './../src/client/write_fileclient');
-    f1.main(req,res,fs)
- });
-
- app.get('/api/client/v1/sync_write_fileclient/',function(req, res){
-    let f1 = require(__dirname + './../src/client/sync_write_fileclient');
-    f1.main(req,res,fs)
- });
-
- app.get('/api/client/v1/read_fileclient/',function(req, res){
-    let f1 = require(__dirname + './../src/client/read_fileclient');
-    f1.main(req,res,fs)
- });
-
- app.get('/api/client/v1/delete_fileclient/',function(req, res){
-    let f1 = require(__dirname + './../src/client/delete_fileclient');
-    f1.main(req,res,fs)
- });
-
- app.get('/api/client/v1/all_functions/',function(req, res){
-    let f1 = require(__dirname + './../src/client/all_functions');
-    f1.main(req,res,fs)
- });
-
-
-// app.delete('/api/client/v1/delete_client/',function(req, res){
-//    let create = require(__dirname + './../src/client/remove_fileclient');
-//    create.main(req,res,fs);
-// });
-// }
+module.exports = function(app){
+   app.post('/api/client/v1/create_client/', function(req, res){
+   });
+   
+   app.get('/api/client/v1/read_client/', function(req, res){
+   
+       let f1 = require(__dirname + './../src/client/read_client');
+       f1.main(req, res)
+   }); 
+   
+   app.post('/api/client/v1/create_client/', function(req, res){
+   });
+   
+   app.post('/api/client/v1/create_client/', function(req, res){
+   });
+   
+   app.get('/api/client/v1/create_client_in_mongo/',function(req, res){
+       let create = require(__dirname + './../src/client/create_client_in_mongo');
+       create.main(req,res)
+   });
+   }
